2c43abd8b0e472b97f0cc14a4ae234ce71cf8c1b
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-20 10:33:20 -0700

    feat: install.sh: if goto() exists, update it instead of doing nothing

--- install.sh
+++ install.sh
@@ -11,4 +11,6 @@ chmod u+x "${TARGET_SRC_DIR}/src/goto.sh"
 if ! grep -q "goto()" "${TARGET_RC_FILE}"; then
     echo '' >> "${TARGET_RC_FILE}"
     echo "goto() { . ${TARGET_SRC_DIR}/src/goto.sh \"\$@\"; }" >> "${TARGET_RC_FILE}"
+else
+    sed -i "s|^goto().*|goto() { . ${TARGET_SRC_DIR}/src/goto.sh \"\$@\"; }|" "${TARGET_RC_FILE}"
 fi
