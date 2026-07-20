81bf391b62874705ad059a0601d499c4459a5b62
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-20 10:44:22 -0700

    chore: install.sh: rename install.sh to installer.sh

---
+++ installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET_RC_FILE="${HOME}/.zshrc"
+TARGET_BIN_DIR="/usr/local/bin"
+TARGET_SRC_DIR="${TARGET_BIN_DIR}/goto"
+
+mkdir -p "${TARGET_SRC_DIR}"
+cp -r src "${TARGET_SRC_DIR}"
+chmod u+x "${TARGET_SRC_DIR}/src/goto.sh"
+
+if ! grep -q "goto()" "${TARGET_RC_FILE}"; then
+    echo '' >> "${TARGET_RC_FILE}"
+    echo "goto() { . ${TARGET_SRC_DIR}/src/goto.sh \"\$@\"; }" >> "${TARGET_RC_FILE}"
+else
+    sed -i "s|^goto().*|goto() { . ${TARGET_SRC_DIR}/src/goto.sh \"\$@\"; }|" "${TARGET_RC_FILE}"
+fi
+
+source ~/.zshrc
