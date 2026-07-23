12eda95a7d0d77cc1a75a99e6538b7a32b518d5a
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2026-07-23 10:51:17 -0700

    feat: bind.zsh: write __goto_bind()

--- src/lib/bind.zsh
+++ src/lib/bind.zsh
@@ -1,9 +1,42 @@
 #!/usr/bin/env zsh
 
 __goto_bind() {
-    local _BIND_OPTS="${1}"
-    local _BIND_ARGS="${2}"
+    local __key="${GOTO_COMMAND_ARGS[1]}"
+    local __dir="${GOTO_COMMAND_ARGS[2]}"
 
-    echo $_BIND_OPTS
-    echo $_BIND_ARGS
+    if [[ -z "${__key}" ]]; then
+        echo "error: bind: argument key: got empty string"
+        
+        return "${GOTO_RETURN_FAILURE}"
+    fi
+
+    if [[ -z "${__dir}" ]]; then
+        echo "error: bind: argument dir: got empty string"
+        
+        return "${GOTO_RETURN_FAILURE}"
+    fi
+
+    if [[ ! -e "${__dir}" ]]; then
+        echo "error: bind: argument dir: no such file or directory: '${__dir}'"
+        
+        return "${GOTO_RETURN_FAILURE}"
+    fi
+
+    if [[ ! -d "${__dir}" ]]; then
+        echo "error: bind: argument dir: not a directory: '${__dir}'"
+        
+        return "${GOTO_RETURN_FAILURE}"
+    fi
+
+    if ! grep --fixed-string --quiet "${__key}:" "${GOTO_CONFIG_FILE}"; then
+        cat >> "${GOTO_CONFIG_FILE}" <<EOF
+${__key}:${__dir}
+EOF
+    else
+        sed -i '' "/^${__key}:/d" "${GOTO_CONFIG_FILE}"
+
+        cat >> "${GOTO_CONFIG_FILE}" <<EOF
+${__key}:${__dir}
+EOF
+    fi
 }
